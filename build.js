627a44ad2c45c79937348b3cb4ebfb021899d6c6
roemhildtg <roemhildtg@gmail.com>
2016-07-29 12:44:53 -0500

    add build/export scripts and improve package.json

--- build.js
+++ build.js
@@ -1,27 +1,9 @@
-
 var stealTools = require("steal-tools");
+var path = require("path");
 
-stealTools.export({
-	system: {
-		config: __dirname+"/package.json!npm"
-	},
-	outputs: {
-		"+amd": {
-			ignore: false
-		},
-		"+cjs": {
-			ignore: false
-		},
-		"+global-js": {
-			modules: ['app/viewer/viewer'],
-			ignore: false
-		},
-		'+global-css': {}
-	}
-}).catch(function(e){
-
-	setTimeout(function(){
-		throw e;
-	},1);
-
+stealTools.build({
+    config: path.join(__dirname,"package.json!npm")
+},{
+    // bundleSteal: true
+    // bundleAssets: true
 });
