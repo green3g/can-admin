e673c6e9db6806c84d2a53ef5e2e15d4dc9050ea
roemhildtg <roemhildtg@gmail.com>
2017-09-07 08:11:23 -0500

    FIX: checkbox multi doesn't need a "choose a value" checkbox

--- components/checkbox-multi/ViewModel.js
+++ components/checkbox-multi/ViewModel.js
@@ -6,6 +6,23 @@ export default FieldInputMap.extend('CheckboxMulti', {
         Type: DefineList,
         Value: DefineList
     },
+    /**
+     * Properties for the select dropdown. The properties object is similar to that of
+     * `util/field/Field` object, except it includes additional properties to define
+     * the select dropdown behavior.
+     * @parent checkbox-multi.ViewModel.props
+     * @property {checkbox-multi.types.SelectFieldProperty} form-widget/field-components/checkbox-multi.ViewModel.properties properties
+     */
+    properties: {
+        set (props) {
+            if (props.optionsPromise) {
+                props.optionsPromise.then((options) => {
+                    props.set('options', options);
+                });
+            } 
+            return props;
+        }
+    },
     onChange () {
         this.dispatch('fieldchange', [{
             value: this.value
