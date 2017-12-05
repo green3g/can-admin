e2a42b05931f46d9dd96da91bf02e001c7c85afe
roemhildtg <roemhildtg@gmail.com>
2017-12-05 09:15:13 -0600

    fix toast helper

--- helpers/message.js
+++ helpers/message.js
@@ -4,7 +4,7 @@ import {TOPICS as T} from 'can-admin/app/constants';
 
 export const message = {
     error (m) {
-        topic.publish(T.ADD_TOAST, {heading: 'Oops!', body: m, severity: 'danger', autoHide: 10000});
+        topic.publish(T.ADD_TOAST, {heading: 'Oops!', body: m, severity: 'error', autoHide: 10000});
     },
     success (m) {
         topic.publish(T.ADD_TOAST, {heading: 'Success!', body: m, severity: 'success', autoHide: 10000});
